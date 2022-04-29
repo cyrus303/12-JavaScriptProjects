3075f320f01e0344f394e9707dbe8920cbb75326
cyrus303 <sachinsunny303@gmail.com>
2022-04-29 06:42:14 +0100

    pushing color flipper

---
+++ 01-color-flipper/app.js
@@ -0,0 +1,15 @@
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  //get random number between 0 and 3
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+
+  color.textContent = colors[randomNumber];
+  document.body.style.backgroundColor = colors[randomNumber];
+});
+
+const getRandomNumber = () => Math.floor(Math.random() * colors.length);
