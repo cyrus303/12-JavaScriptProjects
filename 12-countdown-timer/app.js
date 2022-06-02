09fd36da900faac2a162b6acc620834b28757287
cyrus303 <sachinsunny303@gmail.com>
2022-06-02 07:32:37 +0100

    added automatic countdown function

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -26,7 +26,7 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2022, 4, 24, 17, 30, 0);
+let futureDate = new Date(2022, 5, 3, 7, 39, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -36,4 +36,67 @@ let month = futureDate.getMonth();
 let date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 
-giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${months[month]} ${year} ${hours}:${min}pm`;
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${months[month]} ${year} ${hours}:${min}am`;
+
+// future time in milliseconds
+const futureTime = futureDate.getTime();
+// console.log(futureTime);
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  // console.log(today);
+  const dateDiff = futureTime - today;
+  // console.log(dateDiff);
+
+  // * 1s = 1000ms
+  // * 1m = 60s
+  // * 1hr = 60min
+  // * 1d = 24hr
+
+  //  values in milliseconds
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  const oneSecond = 1000;
+
+  // Calculating values
+  let days = dateDiff / oneDay;
+  days = Math.floor(days);
+  // console.log(days);
+
+  let hours = (dateDiff % oneDay) / oneHour;
+  hours = Math.floor(hours);
+  // console.log(hours);
+
+  let minutes = (dateDiff % oneHour) / oneMinute;
+  minutes = Math.floor(minutes);
+  // console.log(minutes);
+
+  let seconds = (dateDiff % oneMinute) / oneSecond;
+  seconds = Math.floor(seconds);
+  // console.log(seconds);
+
+  // set value array
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+  if (dateDiff < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class = 'expired'>Sorry this giveaway has expired</h4>`;
+  }
+}
+
+// count down
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
